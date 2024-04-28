535ddbc7c933d15127a60737037aa0ab49e8ceb4
subha-129 <166942600+subha-129@users.noreply.github.com>
2024-04-28 05:08:17 +0000

    datatypes completed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use script" // treat all js code as newer version
+// alert(3+3) // this will give error because of nodejs which is not run in browser.
+
+
+// All data types 
+let name="Shuvo Bangal";
+let age=22;
+
+console.log(typeof age);
+
+// number
+//bigint
+//string--""
+//boolean--true/false
+//symbol--unique
+// null--> standalone value-->object
+//undefined ---> undefined
+
+//object
